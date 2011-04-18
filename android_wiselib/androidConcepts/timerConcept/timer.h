0440eef8a1763f99262b83b5ea8fa79a3b3a07c7
Filippos Vasilakis <vasilakisfil@gmail.com>
2011-04-18 21:39:48 +0300

    Updated timer.h..unfortunately due to static members i have to create 2 classes just like pc_timer.h

--- android_wiselib/androidConcepts/timerConcept/timer.h
+++ android_wiselib/androidConcepts/timerConcept/timer.h
@@ -4,9 +4,15 @@
 #include <iostream>
 #include <ctime>
 #include <sys/time.h>
+#include <signal.h>
 #include <stdint.h>
 #include "delegate.hpp"
+
+#define MAX_EVENTS 3
+
 using namespace std;
+
+
 
 //implementation of timer concept in android platform
 class AndroidTimerModel
@@ -22,13 +28,12 @@ public:
 	template<typename T, void (T::*TMethod)(void*)>
 	int set_timer( millis_t millis, T *obj_pnt, void *userdata );
 	//method for just testing purpose TO BE REMOVED
-	int callFirst(void);
+	static int callFirst(void);
+	static void signalHandler(int signal);
 private:
 	//private method that is called by set_timer to add an event to the timer
 	int insert(millis_t millis, timer_delegate_t callback, void *userdata);
 
-	//timer capacity
-	int MAX_EVENTS;
 	//struct that holds the necessary info for every event
 	struct Resources{
 		timer_delegate_t membFunct;
@@ -37,13 +42,13 @@ private:
 		millis_t time;
 	};
 	//declaring an array of 10 Resources structs
-	struct Resources ResData[10];
+	static struct Resources ResData[10];
+	static int best;
 };
 
-//constructor that initializes all the necessary variables
 AndroidTimerModel::AndroidTimerModel()
 {
-	MAX_EVENTS=3;
+	signal(SIGALRM,signalHandler);
 
 	for(int i=0; i<MAX_EVENTS; i++)
 	{
@@ -52,8 +57,9 @@ AndroidTimerModel::AndroidTimerModel()
 		ResData[i].time=-1;
 	}
 
-
 }
+
+
 //template method that sets a new event in the timer concept
 template<typename T, void (T::*TMethod)(void*)>
 int AndroidTimerModel:: set_timer(millis_t millis, T* obj, void *userdata)
@@ -78,11 +84,28 @@ int AndroidTimerModel::insert(millis_t millis, timer_delegate_t callback, void *
 		}
 	}
 
+	itimerval timerval;
+	timerval.it_interval.tv_sec=0;
+	timerval.it_interval.tv_usec=0;
+	timerval.it_value.tv_sec = millis / 1000;
+	timerval.it_value.tv_usec = (millis % 1000) * 1000000;
+	setitimer(ITIMER_REAL,&timerval,NULL);
+
 	return 0;
 
 }
+
+void AndroidTimerModel::signalHandler(int signal)
+{
+	if(signal==SIGALRM)
+	{
+		callFirst();
+	}
+
+}
+
 //method for just testing purpose TO BE REMOVED
-int AndroidTimerModel::callFirst()
+int AndroidTimerModel::callFirst(void)
 {
 	int temp=33;
 
@@ -93,6 +116,7 @@ int AndroidTimerModel::callFirst()
 			(ResData[i].membFunct)(ResData[i].data);
 		}
 	}
+
 
 	return temp;
 
--- android_wiselib/androidConcepts/timerConcept/timerTest.cpp
+++ android_wiselib/androidConcepts/timerConcept/timerTest.cpp
@@ -3,6 +3,7 @@
 #include <sys/time.h>
 #include <stdint.h>
 #include <stdlib.h>
+#include <signal.h>
 #include <string.h>
 #include "delegate.hpp"
 #include "timer.h"
@@ -13,22 +14,27 @@ using namespace std;
 int main()
 {
 
-	typedef uint32_t millis_t;
-	AndroidTimerModel timer;
-	testClass test;
-	char *pointer = (char *) malloc(sizeof(50));
-	strncpy(pointer,":)", sizeof(":)"));
-	uint32_t timeval=15;
+	//AndroidTimerModel::ResData[10];
+	//typedef uint32_t millis_t;
+	//AndroidTimerModel timer;
+	//testClass test;
+	//char *pointer = (char *) malloc(sizeof(50));
+	//strncpy(pointer,":)", sizeof(":)"));
+	//uint32_t timeval=15;
 
-	test.helloWorld(pointer);
+	//test.helloWorld(pointer);
 
 	//cout << pointer << endl << endl;
 
-	strncpy(pointer,":) This time the function was called by timer :)", sizeof(":) This time the function was called by timer :)"));
+	//strncpy(pointer,":) This time the function was called by timer :)", sizeof(":) This time the function was called by timer :)"));
 
-	timer.set_timer<testClass, &testClass::helloWorld>(timeval, &test, pointer);
+	//timer.set_timer<testClass, &testClass::helloWorld>(timeval, &test, pointer);
 
-	timer.callFirst();
+	//timer.callFirst();
+
+	//test();
+
+	while(1) { }
 
 
 
