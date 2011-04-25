ae7ed3d712a1145f6a5c7948126dab9f87fdb2dc
Nikos Triantafyllis <intergallactic2000@yahoo.gr>
2011-04-25 14:58:08 -0700

    Edited nal/bluetooth/test/udp_client.c via GitHub

--- nal/bluetooth/test/udp_client.c
+++ nal/bluetooth/test/udp_client.c
@@ -1,20 +1,3 @@
-/******************************************************************************
-* This file is part of cbox.
-* 
-* cbox is free software: you can redistribute it and/or modify
-* it under the terms of the GNU LesserGeneral Public License as published
-* by the Free Software Foundation, either version 3 of the License, or
-* any later version.
-* 
-* Cbox is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU Lesser General Public License for more details.
-* 
-* You should have received a copy of the GNU Lesser General Public License
-* along with cbox.  If not, see <http://www.gnu.org/licenses/>.
-*******************************************************************************/
-
 /*UDP CLIENT*/
 #include <stdio.h>
 #include <stdlib.h>
