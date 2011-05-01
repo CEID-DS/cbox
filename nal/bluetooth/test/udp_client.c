ebcd15be60eadd01da5ed2e18f43fd25e83c9250
Nikos Triantafyllis <intergallactic2000@yahoo.gr>
2011-05-01 15:52:50 -0700

    Edited nal/bluetooth/test/udp_client.c via GitHub

--- nal/bluetooth/test/udp_client.c
+++ nal/bluetooth/test/udp_client.c
@@ -3,11 +3,11 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
-/* defines STDIN_FILENO, system calls,etc */
-#include <sys/types.h> /* system data type definitions */
-#include <sys/socket.h> /* socket specific definitions */
-#include <netinet/in.h> /* INET constants and stuff */
-#include <arpa/inet.h> /* IP address conversion stuff */
+
+#include <sys/types.h> 
+#include <sys/socket.h>
+#include <netinet/in.h> 
+#include <arpa/inet.h> 
 #include <netdb.h>
 
 #define HOST "127.0.0.1"
@@ -22,21 +22,21 @@ char buf[BUFFER_SIZE];
 int n_sent;
 int n_read;
 
-	//socket
+	
 if ((sk = socket( PF_INET, SOCK_DGRAM, 0 )) < 0){
 	printf("Problem creating socket\n");
 	exit(1);
 	}
 
-	//connection's conf
+	
 server.sin_family=AF_INET;
 server.sin_addr.s_addr=inet_addr(HOST);
-server.sin_port = htons(PORT);/* establish the server port number - we must use network byte order! */
+server.sin_port = htons(PORT);
 
 
 strcpy(buf, "testing\0");
 
-/* send it to the echo server */
+
 n_sent = sendto(sk,buf,BUFFER_SIZE,0,
 (struct sockaddr*) &server,sizeof(server));
 
