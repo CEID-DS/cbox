7b2d82116f2ecbaec1b3202a0ab7e5379721a15a
Nikos Triantafyllis <intergallactic2000@yahoo.gr>
2011-05-01 15:53:51 -0700

    Edited nal/bluetooth/test/udp_server.c via GitHub

--- nal/bluetooth/test/udp_server.c
+++ nal/bluetooth/test/udp_server.c
@@ -4,10 +4,9 @@
 #include <unistd.h>
 #include <sys/types.h> 
 #include <sys/socket.h>
-#include <arpa/inet.h> /* for inet_Addr etc*/
+#include <arpa/inet.h> 
 #include <strings.h>
 
- /*predefined values */
 #define PORT 5000
 #define BUFFER_SIZE 1024
 
@@ -19,20 +18,20 @@ int main()
    struct sockaddr_in from;
    char buf[BUFFER_SIZE];
 
-	//socket
+	
    sock=socket(AF_INET, SOCK_DGRAM, 0);
    if (sock < 0) error("Opening socket");
 
-	//init struct
+	
    length = sizeof(server);
    bzero(&server,length);
 
-	//connection's conf
+
    server.sin_family=AF_INET;
    server.sin_addr.s_addr=INADDR_ANY;
    server.sin_port=htons(PORT);
 
-	//bind the socket
+
    if (bind(sock,(struct sockaddr *)&server,length)<0) perror("binding");
 
 
@@ -41,8 +40,5 @@ int main()
               n = recvfrom(sock,&buf,BUFFER_SIZE,0,(struct sockaddr *)&from,&length);
               if (n < 0) error("recvfrom");
 	      printf("buf: %s\n",buf);
-
-     //  n = sendto(sock,"Got your message\n",17,0,(struct sockaddr *)&from,fromlen);
-      // if (n  < 0) error("sendto");
    	     }
  } 
