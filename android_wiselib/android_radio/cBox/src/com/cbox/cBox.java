a6d59d08174040775887458e0afad1f24af51bf7
Filippos Vasilakis <vasilakisfil@gmail.com>
2011-05-19 02:12:11 +0300

    Fixed jni connection from native to UdpSendNative method..

--- android_wiselib/android_radio/cBox/src/com/cbox/cBox.java
+++ android_wiselib/android_radio/cBox/src/com/cbox/cBox.java
@@ -1,6 +1,10 @@
 package com.cbox;
 
+//import android.R;
+//import android.R;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -12,6 +16,7 @@ public class cBox extends Activity {
     WiseLib wiseLib = new WiseLib();
     Button jnicall = null;
     private String nativeget= null;
+    Object passing = this;
     
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,7 +40,7 @@ public class cBox extends Activity {
 		public void onClick(View v) {
 			// TODO Auto-generated method stub
 			
-			nativeget=wiseLib.androidSend(this);
+			nativeget=wiseLib.androidSend(passing);
 			Toast.makeText(getBaseContext(), nativeget,Toast.LENGTH_SHORT).show();
 		}
 	};
@@ -44,4 +49,23 @@ public class cBox extends Activity {
 	public void UdpSendNative(String s){
 		Toast.makeText(getBaseContext(),s,Toast.LENGTH_SHORT).show();
 	}
+	
+    public void debugFromNative(String s)
+    {
+    	
+    	// prepare the alert box
+        AlertDialog.Builder alertbox = new AlertDialog.Builder(this);
+        // set the message to display
+        alertbox.setMessage(s);
+        // add a neutral button to the alert box and assign a click listener
+        alertbox.setNeutralButton("Ok", new DialogInterface.OnClickListener() {
+            // click listener on the alert box
+            public void onClick(DialogInterface arg0, int arg1) {
+                // the button was clicked
+                //Toast.makeText(getApplicationContext(), "OK button clicked", Toast.LENGTH_LONG).show();
+            }
+        });
+        // show it
+        alertbox.show();
+    }
 }
